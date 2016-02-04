2d2880db2b62ab5c3364d54ce13bdb32376e35b2
Benjamin Coe <ben@npmjs.com>
2016-02-04 15:59:48 -0800

    ben's working environment

--- roles/newww/files/start.sh
+++ roles/newww/files/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 { cd /etc/npme/node_modules/newww; node server.js; } &
-{ cd /etc/npme/npm-inc/npmo-auth-callbacks; node bin/npmo-auth-callbacks.js start --certificate=$CERTIFICATE --redis=$REDIS_URL; } &
+# { cd /etc/npme/npm-inc/npmo-auth-callbacks; node bin/npmo-auth-callbacks.js start --certificate=$CERTIFICATE --redis=$REDIS_URL; } &
 wait -n
 kill 0
 exit 1
