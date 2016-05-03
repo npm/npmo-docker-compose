99f288516fa717493b8cf0ae72f31c8900b4f4dc
Benjamin Coe <ben@npmjs.com>
2016-05-03 10:00:03 -0700

    feat: development environment now works using native osx docker

--- roles/newww/files/start.sh
+++ roles/newww/files/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 { cd /etc/npme/node_modules/@npm/npmo-web-proxy; node ./bin/npmo-web-proxy.js start --auth-host=$AUTH_HOST; } &
-{ cd /etc/npme/node_modules/newww; node server.js; } &
-{ cd /etc/npme/node_modules/@npm/npmo-auth-callbacks; node bin/npmo-auth-callbacks.js start --certificate=$CERTIFICATE --redis=$REDIS_URL; } &
+{ cd /etc/npme/node_modules/@npm/website; node server.js; } &
+{ cd /etc/npme/node_modules/oauth2-server-pg; npm run migrate -- up; node bin/oauth2-server-pg.js start --private-routes; } &
+{ cd /etc/npme/node_modules/@npm/npmo-auth-callbacks; node bin/npmo-auth-callbacks.js start --certificate=$CERTIFICATE --redis=$REDIS_URL --entity-id=$ENTITY_ID --assert-endpoint=$ASSERT_ENDPOINT --logout-endpoint=$LOGOUT_ENDPOINT --nameid-format=$NAMEID_FORMAT --sso-login-url=$SSO_LOGIN_URL --sso-logout-url=$SSO_LOGOUT_URL; } &
+{ cd /etc/npme/node_modules/@npm/annotation-api; node bin/annotation-api.js start --redis-url=$REDIS_URL; } &
 wait -n
 kill 0
 exit 1
--- roles/rr-follower/files/bootstrap.js
+++ roles/rr-follower/files/bootstrap.js
@@ -1,10 +1,21 @@
 var knex = require('knex')
 var c = knex({client: 'pg', connection: {driver: 'pg', user: 'postgres', host: 'postgres'}})
 c.raw('CREATE DATABASE registry_relational')
-.then(function (o) {
-  process.exit(0)
-})
-.catch(function (e) {
-  if (e.code === '42P04') process.exit(0)
-  else process.exit(1)
-})
+  .then(function () {
+    return c.raw('CREATE DATABASE oauth2_server')
+  })
+  .then(function () {
+    process.exit(0)
+  })
+  .catch(function (e) {
+    if (e.code === '42P04') {
+      c.raw('CREATE DATABASE oauth2_server')
+        .then(function () {
+          process.exit(0)
+        })
+        .catch(function () {
+          if (e.code === '42P04') process.exit(0)
+          else process.exit(1)
+        })
+    } else process.exit(1)
+  })
